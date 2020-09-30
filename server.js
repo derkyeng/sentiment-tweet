95e04a87b4b2379c9da8c012aba4bdbd8c71c66d
Derek Yang <dyang@hamilton.edu>
2020-09-30 12:22:40 -0500

    deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const Twit = require('twit')
 require('dotenv').config();
 
 const port = 5000
-app.listen(process.env.PORT || 5000, () => console.log(`Listening on ${port}`))
+app.listen(process.env.PORT || 8080, () => console.log(`Listening on ${port}`))
 app.use(express.json());
 
 // Routes
